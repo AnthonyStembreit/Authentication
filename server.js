dd644a9c0137414db8f19455cd35edda43ca6324
AnthonyStembreit <stembreit@gmail.com>
2021-08-01 10:14:37 -0500

    server confirmed working on localhost

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const sequelize = require('./config/connection/connection');
 
-const routes = require('./routes');
+// const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(routes);
+// app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => {
