28dd744262ae6eaf0e4571aed5d696c6c769e9bb
AnthonyStembreit <stembreit@gmail.com>
2021-08-01 10:23:42 -0500

    added two routes for login and signup, ndoes not return yet

---
+++ routes/apiRoutes.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+
+router.post("/api/signup", async (req, res) => {
+    console.log(req.body)
+    let newUser = Users.create(req.body)
+    console.log(newUser)
+})
+
+router.post("/api/login", async (req, res) => {
+    console.log(req.body)
+    let user = Users.findOne({
+        where:{
+            email: req.body.email
+        }
+    })
+    console.log(user)
+})
