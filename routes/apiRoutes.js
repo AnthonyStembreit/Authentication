7c3d349882464c8dd4f71616fd96ef473677c834
AnthonyStembreit <stembreit@gmail.com>
2021-08-03 16:49:46 -0500

    added html routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -6,12 +6,10 @@ var passport = require("../config/passport");
 
 router.post("/api/signup", async (req, res) => {
     try{
-        console.log(req.body)
         let newUser = await User.findOrCreate({
             where:{email:req.body.email},
             defaults:req.body
         });
-        console.log(newUser)
         res.json(newUser)
     } catch(error){
         console.log(error)
@@ -20,19 +18,5 @@ router.post("/api/signup", async (req, res) => {
 router.post("/api/login", passport.authenticate("local"), function(req, res) {
     res.json(req.user);
   });
-// router.post("/api/login", async (req, res) => {
-//     try{
-//         console.log(req.body)
-//         let user = await User.findOne({
-//             where:{
-//                 email: req.body.email
-//             }
-//         })
-//         console.log(user)
-//         res.json(user)
-//     }catch(error){
-//         console.log(error)
-//     }
-// })
 
 module.exports = router
