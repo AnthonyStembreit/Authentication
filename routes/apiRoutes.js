8d02e3304d800360c50d848198292b14fe8bcf65
AnthonyStembreit <stembreit@gmail.com>
2021-08-03 16:52:12 -0500

    added route for loggin user out and to get data from current user

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -5,18 +5,33 @@ const User = require('../models/User')
 var passport = require("../config/passport");
 
 router.post("/api/signup", async (req, res) => {
-    try{
+    try {
         let newUser = await User.findOrCreate({
-            where:{email:req.body.email},
-            defaults:req.body
+            where: { email: req.body.email },
+            defaults: req.body
         });
         res.json(newUser)
-    } catch(error){
+    } catch (error) {
         console.log(error)
     }
 })
-router.post("/api/login", passport.authenticate("local"), function(req, res) {
+router.post("/api/login", passport.authenticate("local"), function (req, res) {
     res.json(req.user);
+});
+
+router.get("/logout", function (req, res) {
+    req.logout();
+    res.redirect("/");
+});
+router.get("/api/user_data", function(req, res) {
+    if (!req.user) {
+      res.json({});
+    } else {
+      res.json({
+        email: req.user.email,
+        name: req.user.name
+      });
+    }
   });
 
 module.exports = router
