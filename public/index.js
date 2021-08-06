fb857c10b49d56dd4b0e02b659129711cb5bfc84
AnthonyStembreit <stembreit@gmail.com>
2021-08-05 20:09:55 -0500

    code spacing

--- public/index.js
+++ public/index.js
@@ -1,5 +1,4 @@
 $(document).ready(()=>{
-    console.log(window.location)
     switch(window.location.pathname){
         case "/login":loginDisplay();
         break;
@@ -9,12 +8,14 @@ $(document).ready(()=>{
         break;
         default:   signupDisplay();
     }
+
     function loginDisplay(){
         $("#signup-nav").removeClass("active-nav");
         $("#form-label").text("Login Here:");
         $("#formBtn").text("Login");
         $("#login-nav").addClass("active-nav");
     }
+    
     function signupDisplay(){ 
         let label = $("<label>Name</label>").attr("for", "name-input");
         $("#formSubmit").prepend(label, $("<input>").attr("id", "name-input"));
